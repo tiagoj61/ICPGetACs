956e31f69bd41f1be0d926a37f8ffeb5925a2e39
Tiago <tiagoj61@gmail.com>
2022-09-06 08:59:53 -0300

    frist send

---
+++ src/main/java/tiago/j61/ICPGetACs/IcpGetACsApplication.java
@@ -0,0 +1,13 @@
+package tiago.j61.ICPGetACs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IcpGetACsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IcpGetACsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tiago/j61/ICPGetACs/IcpGetACsApplicationTests.java
@@ -0,0 +1,13 @@
+package tiago.j61.ICPGetACs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IcpGetACsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
